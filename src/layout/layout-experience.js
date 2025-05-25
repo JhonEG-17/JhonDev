0e3c41e587d26f39a7f370d64eeb52b97c13c04e
JhonEG-17 <jhonataneg@outlook.com>
2025-05-25 00:47:38 -0600

    update: add experience

--- src/layout/layout-experience.js
+++ src/layout/layout-experience.js
@@ -12,7 +12,15 @@ class layoutExperience extends HTMLElement {
                             <h3 class="__card-subtitle icon-embed"> Desarrollador Web | ETN Turistar Lujo</h3>
                             <p class="__card-text"> Mantenimiento y creacion de sitios web, maquetado y estilado de sitios web.</p>
                         </div>
-                    </div>
+                    </li>
+                    <li class="__experience-item">
+                        <img src="src/assets/icons/icons-webp/jegdevstudios-logo.webp" alt="Logo de la empres ETN Turistar Lujo" class="__card-img">
+                        <div class="__experience-content">
+                            <small class="__card-date"> 20 - mayo - 2024 </small>
+                            <h3 class="__card-subtitle icon-embed"> CEO & Dev Web | JEG Dev Studios</h3>
+                            <p class="__card-text">Direccion, gestion y desarrollo de sitios web, maquetado y estilado de sitios web.</p>
+                        </div>
+                    </li>
                 </ul>
             </section>
         `;
